68f21812a95377817971acebfb7db47071a9aa27
shrinivash03 <nivaskvs844102@gmail.com>
2025-04-12 07:23:49 +0530

    added util files

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import cookieParser from "cookie-parser"
 
 const app=express()
 
-
+//when we use middleware or config setting we use app.use 
 app.use(cors({
      origin:process.env.CORS_ORIGIN,
      Credential:true
@@ -17,7 +17,7 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))//files stored in public file and eveyone should have access 
 
-app.use(cookieParser)
+app.use(cookieParser()) 
 
 
 
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,8 +1,37 @@
-const asyncHandler=(()=>{
-
-})
+//import { Next } from "react-bootstrap/esm/PageItem"
+//we are using promise here
+const asyncHandler=(requestHandler)=>{ 
+ (req,res,next)=>{
+    Promise.resolve(requestHandler(req,res,next)).
+    catch((err)=> next(err))
+ }
+}
 
 export {asyncHandler}
 
+//next – a callback function to pass control to the next middleware, or to pass errors.
+//next(err) passes the error to Express's built-in error handler.
 
 
+
+
+
+// const asyncHandler=()=>{}
+// const asyncHandler=(func) => () => {}
+
+// const asyncHandler=(func) => async() => {}
+
+// //we uses try catches
+// const asyncHandler=(fn)=>async(req,res,next)=>{
+
+//     try{
+//         await func(req,res,next)
+
+//     }
+//     catch(error){
+//         res.status(error.code || 500).json({
+//             success:false,
+//             message:error.message
+//         })
+//     }
+// }
